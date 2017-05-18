a1d278a290ea22c9e9e95040ec0e34cb6300fd98
devkbm <kbm0417@nate.com>
2017-05-18 14:12:09 +0900

    init

---
+++ src/main/java/com/like/ConnectClientApplication.java
@@ -0,0 +1,12 @@
+package com.like;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectClientApplication.class, args);
+	}
+}
